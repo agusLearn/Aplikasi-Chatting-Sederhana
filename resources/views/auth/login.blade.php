a198613a59185e53098470416689e8497d9411cf
agus <agusnindo@gmail.com>
2023-05-26 20:47:57 +0700

    Pembuatan layouts Auth untuk tampilan login beserta pembuatan css, masih kurang tampilan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,25 @@
 @extends('layoutsAuth.authApp')
 
 @section('content')
-    <h1>Halaman Layouts Login</h1>
+<div class="wrapper">
+    <div class="title">
+        <h2>LOGIN FORM</h2>
+    </div>
+    <form action="#" method="post">
+        @csrf
+        <div class="group-input-icon">
+            <i class="fa-solid fa-user"></i>
+            <input type="text" name="userame" placeholder="Enter username !">
+        </div>
+        <div class="group-input-icon">
+            <i class="fa-solid fa-lock"></i>
+            <input type="password" name="password" placeholder="Enter password !">
+        </div>
+        <div class="group-forget-register">
+            <a href="#" class="forget-account">forget account</a>
+            <a href="#" class="register-account">register account</a>
+        </div>
+        <button type="submit" class="btn btn-login">Login</button>
+    </form>
+</div>
 @endsection
--- resources/views/layoutsAuth/authApp.blade.php
+++ resources/views/layoutsAuth/authApp.blade.php
@@ -17,21 +17,28 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
+    <!-- fontawesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <!-- Styles -->
+    <link href="{{ asset('css/styleAuth.css') }}" rel="stylesheet">
 </head>
 
 <body>
 
-    @yield('content')
 
+    <div id="layoutApp">
+        @yield('content')
+    </div>
 
     <!-- jquery cdn -->
-    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script> 
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <!-- bootstrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     @stack('javascript')
 </body>
+
 </html>
