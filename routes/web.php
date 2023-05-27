1023dff28777730bcb08ca8424b61f9d02505fae
agus <agusnindo@gmail.com>
2023-05-27 08:04:10 +0700

    membuat route AppController sebagai main route(route utama ketika dibuka link-nya)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/', [AppController::class, 'appChat'])->name('appChat');
 
 Route::get('login-view', [LoginController::class, 'login_view']);
 
